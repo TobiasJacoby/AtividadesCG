15354ff3d24719cb757c926d5a07dab698b3ee87
Tobias Couto Jacoby <TJACOBY89@edu.unisinos.br>
2023-06-27 09:16:32 -0300

    entrega vivencial 2

---
+++ m4_viv2/Exericio8/Origem.cpp
@@ -0,0 +1,355 @@
+﻿/* Hello Triangle - código adaptado de https://learnopengl.com/#!Getting-started/Hello-Triangle
+ *
+ * Adaptado por Rossana Baptista Queiroz
+ * para a disciplina de Processamento Gráfico - Jogos Digitais - Unisinos
+ * Versão inicial: 7/4/2017
+ * Última atualização em 02/03/2022
+ *
+ */
+
+#include <iostream>
+#include <string>
+#include <assert.h>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+
+using namespace std;
+
+// GLAD
+#include <glad/glad.h>
+
+// GLFW
+#include <GLFW/glfw3.h>
+
+//GLM
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "Shader.h"
+
+
+// Protótipo da função de callback de teclado
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+
+// Protótipos das funções
+int numero_vertices = 0;
+int setupGeometry();
+GLFWwindow* initializeGL();
+int setupShader();
+
+// Dimensões da janela (pode ser alterado em tempo de execução)
+const GLuint WIDTH = 1000, HEIGHT = 500;
+
+
+bool rotateX = false, rotateY = false, rotateZ = false;
+const GLchar* vertexShaderSource = "#version 450\n"
+"layout (location = 0) in vec3 position;\n"
+"uniform mat4 model;\n"
+"void main()\n"
+"{\n"
+//...pode ter mais linhas de código aqui!
+"gl_Position = model * vec4(position, 1.0);\n"
+"}\0";
+
+//Códifo fonte do Fragment Shader (em GLSL): ainda hardcoded
+const GLchar* fragmentShaderSource = "#version 450\n"
+"uniform vec4 finalColor;\n"
+"out vec4 color;\n"
+"void main()\n"
+"{\n"
+"color = finalColor;\n"
+"}\n\0";
+
+
+
+// Função MAIN
+int main()
+{
+	GLFWwindow* window = initializeGL();
+
+	GLuint shaderID = setupShader();
+
+	GLuint VAO = setupGeometry();
+
+	glUseProgram(shaderID);
+
+	glm::mat4 model = glm::mat4(1);
+	GLint modelLoc = glGetUniformLocation(shaderID, "model");
+
+	model = glm::rotate(model, glm::radians(90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+	glUniformMatrix4fv(modelLoc, 1, false, glm::value_ptr(model));
+
+	GLint colorLoc = glGetUniformLocation(shaderID, "finalColor");
+	glUniform4f(colorLoc, 0.5f, 0.0f, 0.5f, 1.0f);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClearColor(0.5f, 0.8f, 0.8f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		float angle = (GLfloat)glfwGetTime();
+
+		model = glm::mat4(1);
+		if (rotateX)
+		{
+			model = glm::rotate(model, angle, glm::vec3(1.0f, 0.0f, 0.0f));
+		}
+		else if (rotateY)
+		{
+			model = glm::rotate(model, angle, glm::vec3(0.0f, 1.0f, 0.0f));
+		}
+		else if (rotateZ)
+		{
+			model = glm::rotate(model, angle, glm::vec3(0.0f, 0.0f, 1.0f));
+		}
+
+		glUniformMatrix4fv(modelLoc, 1, false, glm::value_ptr(model));
+
+		// Renderização do modelo da Suzanne
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, (numero_vertices * 3));
+		glUniform4f(colorLoc, 1.0, 1.0, 1.0, 1.0);
+
+		for (int n = 0; n < numero_vertices; n += 3) {
+			glDrawArrays(GL_LINE_LOOP, n, 3);
+		}
+		glUniform4f(colorLoc, 0.0f, 0.0f, 0.0f, 1.0f);
+
+		glBindVertexArray(0);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	glDeleteVertexArrays(1, &VAO);
+	glfwTerminate();
+	return 0;
+}
+
+// Função de callback de teclado - só pode ter uma instância (deve ser estática se
+// estiver dentro de uma classe) - é chamada sempre que uma tecla for pressionada
+// ou solta via GLFW
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
+
+	if (key == GLFW_KEY_X && action == GLFW_PRESS)
+	{
+		rotateX = true;
+		rotateY = false;
+		rotateZ = false;
+	}
+
+	if (key == GLFW_KEY_Y && action == GLFW_PRESS)
+	{
+		rotateX = false;
+		rotateY = true;
+		rotateZ = false;
+	}
+
+	if (key == GLFW_KEY_Z && action == GLFW_PRESS)
+	{
+		rotateX = false;
+		rotateY = false;
+		rotateZ = true;
+	}
+
+}
+
+// Esta função está bastante harcoded - objetivo é criar os buffers que armazenam a 
+// geometria de um triângulo
+// Apenas atributo coordenada nos vértices
+// 1 VBO com as coordenadas, VAO com apenas 1 ponteiro para atributo
+// A função retorna o identificador do VAO
+int setupGeometry()
+{
+	std::vector< glm::vec3 > vertexIndices = {};
+	std::vector< glm::vec3 > triangulos = {};
+	string line;
+	string v, valuesX, valuesY, valuesZ;
+
+	//ifstream myfile("3D_Models/Cube/cube.obj");
+	//ifstream myfile("3D_Models/Suzanne/suzanneTri.obj");
+	ifstream myfile("3D_Models/Suzanne/bola.obj");
+	//ifstream myfile("images/suzanneTri.obj");
+	while (!myfile.eof())
+	{
+		getline(myfile, line);
+		std::istringstream iss(line);
+		if (line[0] == 'v' && line[1] == ' ') {
+			iss >> v >> valuesX >> valuesY >> valuesZ;
+			glm::vec3 aux = { std::stof(valuesX), std::stof(valuesY), std::stof(valuesZ) };
+			vertexIndices.push_back(aux);
+		}
+
+		if (line[0] == 'f' && line[1] == ' ')
+		{
+
+			string delimiter = " ";
+			string delimiter1 = "/";
+			int primeira_parte = 0;
+			int segunda_parte = 0;
+			int terceira_parte = 0;
+			auto finish = line.find(delimiter); // gives position of first occurrence of delimiter
+			line = line.substr(finish + 1);
+
+			if (finish != line.npos) { // find returned the end of string
+				primeira_parte = stoi(line.substr(0, line.find(delimiter1))); // 
+			}
+
+			finish = line.find(delimiter); // gives position of first occurrence of delimiter
+			line = line.substr(finish + 1);
+
+			if (finish != line.npos) { // find returned the end of string
+				segunda_parte = stoi(line.substr(0, line.find(delimiter1))); // 
+			}
+
+			finish = line.find(delimiter); // gives position of first occurrence of delimiter
+			line = line.substr(finish + 1);
+
+			if (finish != line.npos) { // find returned the end of string
+				terceira_parte = stoi(line.substr(0, line.find(delimiter1))); // 
+			}
+
+			triangulos.push_back(vertexIndices[terceira_parte - 1]);
+			triangulos.push_back(vertexIndices[segunda_parte - 1]);
+			triangulos.push_back(vertexIndices[primeira_parte - 1]);
+
+
+			numero_vertices++;
+		}
+	}
+
+	GLuint VBO, VAO;
+
+	//Geração do identificador do VBO
+	glGenBuffers(1, &VBO);
+
+	//Faz a conexão (vincula) do buffer como um buffer de array
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	//Envia os dados do array de floats para o buffer da OpenGl
+	glBufferData(GL_ARRAY_BUFFER, triangulos.size() * sizeof(GLfloat) * 3, &triangulos[0], GL_STATIC_DRAW);
+
+	//Geração do identificador do VAO (Vertex Array Object)
+	glGenVertexArrays(1, &VAO);
+
+	// Vincula (bind) o VAO primeiro, e em seguida  conecta e seta o(s) buffer(s) de vértices
+	// e os ponteiros para os atributos 
+	glBindVertexArray(VAO);
+
+	//Para cada atributo do vertice, criamos um "AttribPointer" (ponteiro para o atributo), indicando: 
+	// Localização no shader * (a localização dos atributos devem ser correspondentes no layout especificado no vertex shader)
+	// Numero de valores que o atributo tem (por ex, 3 coordenadas xyz) 
+	// Tipo do dado
+	// Se está normalizado (entre zero e um)
+	// Tamanho em bytes 
+	// Deslocamento a partir do byte zero 
+
+	//Atributo posição (x, y, z)
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+
+	//Atributo cor (r, g, b)
+	//glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+	//glEnableVertexAttribArray(1);
+
+
+
+	// Observe que isso é permitido, a chamada para glVertexAttribPointer registrou o VBO como o objeto de buffer de vértice 
+	// atualmente vinculado - para que depois possamos desvincular com segurança
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	// Desvincula o VAO (É uma boa prática desvincular qualquer buffer ou array para evitar bugs medonhos)
+	glBindVertexArray(0);
+
+	return VAO;
+}
+
+GLFWwindow* initializeGL()
+{
+	// Inicialização da GLFW
+	if (!glfwInit())
+	{
+		std::cout << "Failed to initialize GLFW" << std::endl;
+		return nullptr;
+	}
+
+	// Configuração do contexto OpenGL
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// Criação da janela GLFW
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Examples TJ", nullptr, nullptr);
+	if (!window)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return nullptr;
+	}
+
+	glfwMakeContextCurrent(window);
+	glfwSetKeyCallback(window, key_callback);
+
+	// Inicialização do GLAD
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return nullptr;
+	}
+
+	// Definindo as dimensões da viewport com as mesmas dimensões da janela da aplicação
+	int width, height;
+	glfwGetFramebufferSize(window, &width, &height);
+	glViewport(0, 0, width, height);
+
+	return window;
+}
+
+int setupShader()
+{
+	// Vertex shader
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+	// Checando erros de compilação (exibição via log no terminal)
+	GLint success;
+	GLchar infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+	// Fragment shader
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+	// Checando erros de compilação (exibição via log no terminal)
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+	// Linkando os shaders e criando o identificador do programa de shader
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	// Checando por erros de linkagem
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+	}
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+
+	return shaderProgram;
+}
